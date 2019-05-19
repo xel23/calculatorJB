55da730e0cceb94e2a92b8f40c0d595bd60f5544
Igor <sawqe1@yandex.ru>
2019-05-19 14:05:47 +0300

    add Top down operator precedence for parser

--- calc.js
+++ calc.js
@@ -30,6 +30,7 @@ let lex = function (input) {
     };
 
     while (i < input.length) {
+        c = input[i];
         // if c is whitespace -> skip
         if (isWhiteSpace(c)) {
             advance();
@@ -69,10 +70,103 @@ let lex = function (input) {
     return tokens;
 };
 
-let parser = function (tokens) {
+
+// Top down operator precedence
+// https://crockford.com/javascript/tdop/tdop.html
+let parse = function(tokens) {
     let parseTree = [];
 
+    let symbols = {},
+        symbol = function(id, lbp, nud, led) {
+            if (!symbols[id]) {
+                symbols[id] = {
+                    lbp: lbp,
+                    nud: nud,
+                    led: led
+                };
+            }
+            else {
+                if (nud) symbols[id].nud = nud;
+                if (led) symbols[id].led = led;
+                if (lbp) symbols[id].lbp = lbp;
+            }
+        };
 
+    let interpretToken = function(token) {
+        let Func = function() {};
+        Func.prototype = symbols[token.type];
+        let sym = new Func;
+        sym.type = token.type;
+        sym.value = token.value;
+        return sym;
+    };
 
+    let i = 0;
+    let token = function() {
+        return interpretToken(tokens[i]);
+    };
+    let advance = function() {
+        i++;
+        return token();
+    };
+
+    let expression = function(rbp) {
+        let left, t = token();
+        advance();
+        if (!t.nud) throw "Unexpected token: " + t.type;
+        left = t.nud(t);
+        while (rbp < token().lbp) {
+            t = token();
+            advance();
+            if (!t.led) throw "Unexpected token: " + t.type;
+            left = t.led(left);
+        }
+        return left;
+    };
+
+    let infix = function(id, lbp, rbp, led) {
+        rbp = rbp || lbp;
+        symbol(id, lbp, null, led ||
+            function(left) {
+                return {
+                    type: id,
+                    left: left,
+                    right: expression(rbp)
+                };
+            });
+    };
+
+    let prefix = function(id, rbp, nud) {
+        symbol(id, null, nud ||
+            function() {
+                return {
+                    type: id,
+                    right: expression(rbp)
+                };
+            });
+    };
+
+    prefix("number", 7, function(number) {
+        return number;
+    });
+
+    prefix("(", 6, function() {
+        let value = expression(0);
+        if (token().type !== ")") throw "Expected closing parenthesis ')'";
+        advance();
+        return value;
+    });
+
+    prefix("-", 5);
+    infix("^", 4, 3);
+    infix("*", 2);
+    infix("/", 2);
+    infix("%", 2);
+    infix("+", 1);
+    infix("-", 1);
+
+    while (token().type !== "(end)") {
+        parseTree.push(expression(0));
+    }
     return parseTree;
 };
